561edcc7f8c94c97da6db07ece9dff058cef9f08
Zeeder <i_be_pretty@yahoo.com>
2017-04-11 13:02:07 -0400

    users table page working no photos

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,45 @@
-<h1>Users Index working</h1>
+@extends('layouts.admin')
+
+@section('content')
+  <h1>Users</h1>
+
+  <div class="container">
+  <h2>Table</h2>
+  <p>The .table-responsive class creates a responsive table which will scroll horizontally on small devices (under 768px). When viewing on anything larger than 768px wide, there is no difference:</p>
+  <div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Role</th>
+        <th>Status</th>
+        <th>Created</th>
+        <th>Updated</th>
+      </tr>
+    </thead>
+    <tbody>
+      @if($users)
+        @foreach($users as $user)
+      <tr>
+        <td>{{$user->id}}</td>
+        <td>{{$user->name}}</td>
+        <td>{{$user->email}}</td>
+        <td>{{$user->role['name']}}</td>
+        <td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
+        <td>{{$user->created_at->diffForHumans()}}</td>
+        <td>{{$user->updated_at->diffForHumans()}}</td>
+      </tr>
+        @endforeach
+      @endif
+
+    </tbody>
+  </table>
+  </div>
+</div>
+
+
+
+
+@stop
