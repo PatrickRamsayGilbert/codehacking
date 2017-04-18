f95bba42d0ed5ccf30434c62383490ee5b248883
Zeeder <i_be_pretty@yahoo.com>
2017-04-18 11:29:15 -0400

    users section working minus security

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -10,7 +10,8 @@
   <table class="table">
     <thead>
       <tr>
-        <th>#</th>
+        <th>Id</th>
+        <th>Photo</th>
         <th>Name</th>
         <th>Email</th>
         <th>Role</th>
@@ -24,7 +25,8 @@
         @foreach($users as $user)
       <tr>
         <td>{{$user->id}}</td>
-        <td>{{$user->name}}</td>
+        <td><img height="50" src="{{$user->photo ? $user->photo->file : 'http://placehold.it/400x400'}}" alt=""></td>
+        <td><a href="{{route('admin.users.edit', $user->id)}}">{{$user->name}}</td>
         <td>{{$user->email}}</td>
         <td>{{$user->role['name']}}</td>
         <td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
